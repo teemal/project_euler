ef69af61fee0285208bd2a847ead1af62f7c4954
teemal <taymal1987@gmail.com>
2017-12-05 22:01:36 -0800

    finding largest prime factorial

--- id_3.py
+++ id_3.py
@@ -1,2 +1,15 @@
 #Taylor Mallory
 #project euler
+import math
+
+def is_it_prime(n):
+        for i in range(2, int(math.sqrt(n))):
+            if n % i == 0:
+                return False
+            return True
+#print(is_it_prime(13))
+
+def get_largest_prime_fac(m):
+    holder = m / 2
+    if is_it_prime(holder) != True:
+        
