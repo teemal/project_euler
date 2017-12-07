a83baa59995498b21f47e45b0349f39a86127d5d
teemal <taymal1987@gmail.com>
2017-12-06 22:20:27 -0800

    found largest palindrome from product of 3 digit numbers but will come back for efficiency

--- id_4.py
+++ id_4.py
@@ -1,3 +1,21 @@
 #Taylor Mallory
 #project euler
-#"Find the largest palindrome made from the product of two 3-digit numbers."
+#Find the largest palindrome made from the product of two 3-digit numbers.
+store = []
+def is_palindrome(n):
+    num = str(n)
+    rev = num[::-1]
+    return rev == num
+
+
+for i in range(999,99, -1):
+    for j in range(999,99, -1):
+        test = i * j
+        if is_palindrome(test) == True:
+            print(test)
+            break
+        #if is_palindrome(i * j) == True:
+        #        store.append(i)
+        #        store.append(j)
+        #        print(store)
+        #        break
