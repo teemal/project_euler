306b5b95e191e2bcd374395d078fc0fc015cfc34
teemal <taymal1987@gmail.com>
2017-12-08 17:57:49 -0800

    found prime after 10, now need to iterate to 10001st

--- id_7.py
+++ id_7.py
@@ -2,3 +2,14 @@
 #project euler
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10001st prime number?
+
+import math
+
+def is_it_prime(n):
+    for i in range(2, int(math.sqrt(n))):
+        if n%i == 0:
+            return False
+        else:
+            return True
+
+print(is_it_prime(9))
